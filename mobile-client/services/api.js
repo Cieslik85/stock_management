0f5bf698479fdb084529fa4a657676d13d3a9b47
Cieslik85 <cieslik85@gmail.com>
2025-09-06 12:34:17 +0100

    fixed mobile login

--- mobile-client/services/api.js
+++ mobile-client/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://192.168.1.10:5000'; // e.g., http://192.168.1.10:5000
+const API_URL = 'http://10.0.2.2:5000'; // e.g., http://192.168.1.10:5000
 
 export const login = (email, password) =>
-    axios.post(`${API_URL}/auth/login`, { email, password });
+    axios.post(`${API_URL}/api/auth/login`, { email, password });
 
 export const fetchProducts = (token) =>
     axios.get(`${API_URL}/products`, {
